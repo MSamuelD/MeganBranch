cd8f229791ce5e2ac2e6181e8f24ab6127ead8b8
Sang <angminhsang@gmail.com>
2025-09-25 02:11:20 +1000

    removed unnecessary element from the database to save storage, added a few more fields to Person class

--- PetShop/PetShop/Models/Person.cs
+++ PetShop/PetShop/Models/Person.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +15,14 @@ namespace PetShop.Models
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+        [NotMapped]
         public string Address { get; set; }
         public string City { get; set; }
         public string State { get; set; }
         public string ZipCode { get; set; }
         public string Country { get; set; }
+        public string StreetNumber { get; set; }
+        public string StreetName { get; set; }
         // Empty constructor for EF Core
         public Person() { }
     }
