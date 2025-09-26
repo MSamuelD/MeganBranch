74dce522b5ef0fa38f609a6a11b06a1bf0382910
Sang <angminhsang@gmail.com>
2025-09-26 16:34:11 +1000

    Added appointment class and its required components

---
+++ PetShop/PetShop/Models/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Models
+{
+    public class Appointment
+    {
+        public Appointment() { }
+        public int Id { get; set; }
+        public Timeslot Timeslot { get; set; }
+        public Pet Pet { get; set; }
+        public Customer Customer { get; set; }
+        public string Details { get; set; }
+    }
+}
